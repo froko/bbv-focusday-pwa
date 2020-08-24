39fec03343bdd871f57c3a11a023bd05be54eb38
Patrick Ineichen <github@frokonet.ch>
2020-08-24 18:43:34 +0200

    feat: 🎸 Add jsonbin URLs

--- companion-app/src/environments/environment.prod.ts
+++ companion-app/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 const providers: any[] = [
   { provide: 'environment', useValue: 'Production' },
-  { provide: 'baseUrl', useValue: 'http://localhost:3000' }
+  { provide: 'timeslotUrl', useValue: 'https://api.jsonbin.io/b/5f3a9a494d93991036169e3f' },
+  { provide: 'sessionUrl', useValue: 'https://api.jsonbin.io/b/5f43e462993a2e110d356b66' }
 ];
 
 export const ENV_PROVIDERS = providers;
